d5fd81b42e5b98ee6e05fdc8e72a607da262f443
Adis <2289372@cegepmontpetit.ca>
2024-01-29 17:27:40 -0500

    fo créer des actions or something

---
+++ Migrations/20240129214116_xxx.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RelationsNaN.Migrations
+{
+    /// <inheritdoc />
+    public partial class xxx : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "GameId",
+                table: "Game",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Game_GameId",
+                table: "Game",
+                column: "GameId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Game_Game_GameId",
+                table: "Game",
+                column: "GameId",
+                principalTable: "Game",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Game_Game_GameId",
+                table: "Game");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Game_GameId",
+                table: "Game");
+
+            migrationBuilder.DropColumn(
+                name: "GameId",
+                table: "Game");
+        }
+    }
+}
